b7108bc278be53c9266c0d58211692b457f49b46
alan bount <alan@zeroasterisk.com>
2018-07-30 20:08:32 -0400

    basics installed

--- auditor/mix.exs
+++ auditor/mix.exs
@@ -14,7 +14,7 @@ defmodule Auditor.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :goth],
       mod: {Auditor.Application, []}
     ]
   end
@@ -22,6 +22,11 @@ defmodule Auditor.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:combination, "~> 0.0.3"},
+      {:timex, "~> 3.1"},
+      {:excheck, "~> 0.6", only: :test},
+      {:mix_test_watch, "~> 0.6", only: :dev, runtime: false},
+      {:diplomat, "~> 0.9"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
